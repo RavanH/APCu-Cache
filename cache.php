41bb3029dc8369e9760e248dca157ff31553a725
RavanH <ravanhagen@gmail.com>
2023-12-11 20:42:10 +0100

    add extra check

--- cache.php
+++ cache.php
@@ -1,3 +1,8 @@
 <?php
-// Early loader for APC Cache
-include(YOURLS_PLUGINDIR . '/apcu-cache/plugin.php');
+// Early loader for APCu Cache.
+
+$cache_plugin = YOURLS_PLUGINDIR . '/apcu-cache/plugin.php';
+
+if ( file_exists( $cache_plugin ) && function_exists( 'apcu_exists' ) ) {
+	include( $cache_plugin );
+}
